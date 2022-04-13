635303516c2c198a021c4b6eea37a76f663b8ff7
kevinbennetth <kevinbennott@gmail.com>
2022-04-13 21:56:55 +0700

    update best_listing_time

--- best_listing_time.py
+++ best_listing_time.py
@@ -1,12 +1,11 @@
 import pandas as pd
 import numpy as np
+import dash_bootstrap_components as dbc
 
 best_listing = pd.read_csv('./cleaned-datasets/best_listing.csv')
 
-from dash import Dash, html, dcc
 import plotly.express as px
 
-app = Dash(__name__)
 fig = px.bar(best_listing, x='duration_range', y='count',
             color='duration_range', text='count',
             title='<b>Events observed based on Duration</b>',
@@ -17,13 +16,21 @@ fig = px.bar(best_listing, x='duration_range', y='count',
 fig.update_traces(textfont_size=12, textposition="outside", cliponaxis=False)
 fig.update_layout(showlegend=False)
 
-app.layout = html.Div(children=[
-    html.H1("Best Listing Duration"),
-    dcc.Graph(figure=fig, style={
-        'width': '50%',
-        'height': '500px'
-    })
-])
+from dash import Dash, html, dcc
+app = Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
+app.layout = dbc.Container(children=[
+    html.Div(children=[
+        html.H1("Best Listing Duration"),
+        dcc.Graph(figure=fig, style={
+            'width': '50%',
+            'height': '500px'
+        })
+    ], 
+        style={
+            'font-family': 'Poppins'
+        }
+    )
+], className="p-5")
 
 if __name__ == "__main__":
     app.run_server(debug=True)

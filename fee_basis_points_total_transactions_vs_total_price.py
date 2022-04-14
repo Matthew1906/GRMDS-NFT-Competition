44ea165a4d3ebad0f91eb125163dfbc488fe1595
RyneFerdinand <73730502+RyneFerdinand@users.noreply.github.com>
2022-04-14 17:00:04 +0700

    update

--- fee_basis_points_total_transactions_vs_total_price.py
+++ fee_basis_points_total_transactions_vs_total_price.py
@@ -1,18 +1,19 @@
 from cProfile import label
 import plotly.express as px
-from dash import Dash, dcc, html, Input, Output  # pip install dash (version 2.0.0 or higher)
+from dash import Dash, dcc, html, Input, Output
 from preprocessing import get_assets
 
 app = Dash(__name__, suppress_callback_exceptions=True)
 
 assets = get_assets()
-
+``
 assets_sold = assets[assets['last_sale_created_date'].notna()]
 fig = px.scatter(assets_sold['last_sale_total_price'], 
                 assets_sold['asset_contract_dev_seller_fee_basis_points'],
                 labels={'index':"Last Sale Created Date",
                         'x':'Asset Contract Dev Seller Fee Basis Points'
-                })
+                },
+                trendline="ols")
 
 app.layout = html.Div(children=[
 	html.H1("Fee Basis Points & Total Transactions vs. Total Price"),

82b855c9a79cfe3a0a95d1a001086a0c4f521acc
RyneFerdinand <73730502+RyneFerdinand@users.noreply.github.com>
2022-04-14 17:37:59 +0700

    update

--- fee_basis_points_total_transactions_vs_total_price.py
+++ fee_basis_points_total_transactions_vs_total_price.py
@@ -1,5 +1,6 @@
 from cProfile import label
 import plotly.express as px
+import plotly.subplots as sp
 from dash import Dash, dcc, html, Input, Output
 from preprocessing import get_assets
 
@@ -7,17 +8,43 @@ app = Dash(__name__, suppress_callback_exceptions=True)
 
 assets = get_assets()
 assets_sold = assets[assets['last_sale_created_date'].notna()]
-fig = px.scatter(assets_sold['last_sale_total_price'], 
+
+figure1 = px.scatter(assets_sold['last_sale_total_price'], 
+                assets_sold['asset_contract_dev_seller_fee_basis_points'],
+                color_discrete_sequence=["black"],
+                labels={'index':"Last Sale Created Date",
+                        'x':'Asset Contract Dev Seller Fee Basis Points'
+                },
+                trendline="ols",
+                trendline_color_override="black")
+
+figure2 = px.scatter(assets_sold['last_sale_total_price'], 
                 assets_sold['asset_contract_dev_seller_fee_basis_points'],
                 labels={'index':"Last Sale Created Date",
                         'x':'Asset Contract Dev Seller Fee Basis Points'
                 },
                 trendline="ols")
 
+
+# figure1_traces = []
+# figure2_traces = []
+# for trace in range(len(figure1["data"])):
+#     figure1_traces.append(figure1["data"][trace])
+# for trace in range(len(figure2["data"])):
+#     figure2_traces.append(figure2["data"][trace])
+
+# this_figure = sp.make_subplots(rows=1, cols=2) 
+
+# # Get the Express fig broken down as traces and add the traces to the proper plot within in the subplot
+# for traces in figure1_traces:
+#     this_figure.append_trace(traces, row=1, col=1)
+# for traces in figure2_traces:
+#     this_figure.append_trace(traces, row=1, col=2)
+
 app.layout = html.Div(children=[
 	html.H1("Fee Basis Points & Total Transactions vs. Total Price"),
 	html.Div(
-        dcc.Graph(id="scat_plt", figure=fig)
+        dcc.Graph(id="scat_plt", figure=figure1)
     )
 ])
 

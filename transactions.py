06e9df0e1ed0c5abeec40c169183cfd4abe68ebf
matthew adrianus <matthewadrianus@gmail.com>
2022-04-14 18:19:34 +0700

    Improve NFT Transactions & Preprocessing

--- transactions.py
+++ transactions.py
@@ -4,31 +4,24 @@ from preprocessing import get_assets
 app = Dash(__name__, suppress_callback_exceptions=True)
 
 assets = get_assets()
-top_10_NFT = assets.sort_values('num_sales', ascending=False)[['collection_slug','token_id','name','asset_category','created_year','num_sales']].head(10)
 
 app.layout = html.Div(
     children=[
-        html.H1('NFT Transactions Analysis'),
+        html.H1('NFT Transactions Analysis', id='transaction-heading'),
         html.Div(
-            children = [
-                html.H3("Top 10 NFT Transactions"),
-                dash_table.DataTable(
-                    id='top-10-nft',
-                    data = top_10_NFT.to_dict('records'),
-                    columns = [{'id':col, 'name':col} for col in top_10_NFT.columns]
-                )                
-            ]
-        ),
-        html.Div(
-            id='transaction-choose',
+            id='transaction',
             children=[
-                html.H3(id='transaction-choose-heading'),
                 html.Div(
                     style = {'display':'flex', 'justifyContent':'center', 'alignItems':'center'},
                     children = [
                         dcc.Dropdown(
-                            options = ['asset_category', 'collection_slug', 'created_year'],
-                            value = 'asset_category',
+                            options = [
+                                'Top 10 NFT', 
+                                'Asset Category',
+                                'Collection Slug', 
+                                'Created Year'
+                            ],
+                            value = 'Top 10 NFT',
                             id='transaction-choose-category',
                             style={
                                 'width':'50%'
@@ -49,6 +42,7 @@ app.layout = html.Div(
                         )
                     ]
                 ),
+                html.Br(),
                 html.Div(
                     id='transaction-choose-content'
                 )
@@ -59,15 +53,16 @@ app.layout = html.Div(
 
 @app.callback(
     Output(component_id='transaction-choose-options', component_property='children'),
-    Output(component_id='transaction-choose-heading', component_property='children'),
+    Output(component_id='transaction-heading', component_property='children'),
     Input(component_id='transaction-choose-category', component_property='value')
 )
 def update_choose_options(category):
     return dcc.Dropdown(
-        options = assets[category if category!=None else 'asset_category'].unique(),
-        value = assets[category if category!=None else 'asset_category'].unique()[0],
+        options = sorted(assets[category if category!=None else 'Asset Category'].unique()),
+        value = sorted(assets[category if category!=None else 'Asset Category'].unique())[0],
         id='transaction-choose-option'
-    ), f'Top 10 NFT Transactions based on {category}'
+    ) if category!='Top 10 NFT' else html.Div(id='transaction-choose-option'),\
+    f'Top 10 NFT Transactions based on {category}' if category!='Top 10 NFT' else 'Top 10 NFT Transactions'
 
 @app.callback(
     Output(component_id='transaction-choose-content', component_property='children'),
@@ -75,12 +70,22 @@ def update_choose_options(category):
     Input(component_id='transaction-choose-option', component_property='value')
 )
 def update_result(category, option):
-    selected = assets.loc[assets[category]==option]
-    display = selected.sort_values('num_sales', ascending=False)[['collection_slug','token_id','name','asset_category','num_sales','created_year']].head(10)
+    if category == 'Top 10 NFT':
+        display = assets.sort_values('Num Sales', ascending=False)[['Collection Slug','Name','Asset Category','Created Year','Num Sales']].head(10)
+    else:  
+        selected = assets.loc[assets[category]==option]
+        display = selected.sort_values('Num Sales', ascending=False)[['Collection Slug','Name','Asset Category','Num Sales','Created Year']].head(10)
     return dash_table.DataTable(
+        id='top-10-nft',
         data = display.to_dict('records'),
-        columns = [{'id':col, 'name':col} for col in display.columns]
-    )
+        columns = [{'id':col, 'name':col} for col in display.columns],
+        style_header = {
+            'textAlign':'center'
+        },
+        style_data = {
+            'textAlign':'left'
+        }
+    ) 
 
 if __name__ == "__main__":
     app.run_server(debug=True)
